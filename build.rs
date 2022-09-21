4496c407bf8daa9ca86eda6435cc49089acb9dad
lyt <ahaloer@163.com>
2022-09-22 01:45:18 +0800

    To latest madsim

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
+use std::path::Path;
+
 fn main() -> std::io::Result<()> {
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
     let proto = "proto/proto.proto";
-    tonic_build::compile_protos(proto)?;
+    tonic_build::configure().compile_with_config(config, &["proto/proto.proto"], &["proto"])?;
     println!("cargo:rerun-if-changed={}", proto);
     Ok(())
 }
