340b286780a7ecd2b15bcca25ac7159c5fde6ee0
lyt <ahaloer@163.com>
2022-11-11 16:38:40 +0800

    no arg for proto build

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate cc;
 
 fn main() -> std::io::Result<()> {
     let mut config = prost_build::Config::new();
-    config.protoc_arg("--experimental_allow_proto3_optional");
+    //config.protoc_arg("--experimental_allow_proto3_optional");
     let proto = "proto/proto.proto";
     tonic_build::configure().compile_with_config(config, &["proto/proto.proto"], &["proto"])?;
     println!("cargo:rerun-if-changed={}", proto);
